a77be64d4c54711ba91e9846f68fa01184ef7711
user_os <user_os>
2021-08-25 16:43:19 +0200

    test models

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -43,7 +43,7 @@ DJANGO_APPS = [
 # Third party apps
 THIRD_PARTY_APPS = []
 # Local apps
-LOCAL_APPS = []
+LOCAL_APPS = ["apps.products.apps.ProductConfig"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
